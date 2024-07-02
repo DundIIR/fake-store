95b24281aa7c16faa3adf31a2e2b907a0a474a38
DundIR-PC <ruzin-de@yandex.ru>
2024-07-02 19:32:26 +0500

    Добавлена файловая структура приложения и файлы для работы с Docker

---
+++ src/components/app/App.jsx
@@ -0,0 +1,10 @@
+import Header from '../Header/Header'
+import Filters from '../Filters/Filters'
+import ProductList from '../CardList/CardList'
+import './_app.scss'
+
+function App() {
+	return <div className="app"></div>
+}
+
+export default App
