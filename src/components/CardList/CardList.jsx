8c85a5c069011d32316a05049d6ee0167bde1e96
DundIR-PC <ruzin-de@yandex.ru>
2024-07-10 22:28:16 +0500

    Редактирование файла Readme

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -5,47 +5,47 @@ import image2 from '../../resources/img/image2.png'
 
 const CardList = ({ layout }) => {
 	const products = [
-		// {
-		// 	id: 1,
-		// 	category: "men's clothing",
-		// 	name: 'Fjallraven - Foldsack No. 1 Backpack, Fits 1...',
-		// 	rating: 3.9,
-		// 	price: 7.95,
-		// 	image: image1,
-		// },
-		// {
-		// 	id: 2,
-		// 	category: "men's clothing",
-		// 	name: 'Mens Casual Premium Slim Fit T-Shirts',
-		// 	rating: 4.3,
-		// 	price: 22.3,
-		// 	image: image2,
-		// },
-		// {
-		// 	id: 3,
-		// 	category: "women's clothing",
-		// 	name: "Opna Women's Short Sleeve Moisture",
-		// 	rating: 4.5,
-		// 	price: 137.95,
-		// 	image: image1,
-		// 	favorite: true,
-		// },
-		// {
-		// 	id: 4,
-		// 	category: "women's clothing",
-		// 	name: "Opna Women's Short Sleeve Moisture",
-		// 	rating: 4.5,
-		// 	price: 137.95,
-		// 	image: image1,
-		// },
-		// {
-		// 	id: 5,
-		// 	category: "women's clothing",
-		// 	name: "Opna Women's Short Sleeve Moisture",
-		// 	rating: 4.5,
-		// 	price: 137.95,
-		// 	image: image1,
-		// },
+		{
+			id: 1,
+			category: "men's clothing",
+			name: 'Fjallraven - Foldsack No. 1 Backpack, Fits 1...',
+			rating: 3.9,
+			price: 7.95,
+			image: image1,
+		},
+		{
+			id: 2,
+			category: "men's clothing",
+			name: 'Mens Casual Premium Slim Fit T-Shirts',
+			rating: 4.3,
+			price: 22.3,
+			image: image2,
+		},
+		{
+			id: 3,
+			category: "women's clothing",
+			name: "Opna Women's Short Sleeve Moisture",
+			rating: 4.5,
+			price: 137.95,
+			image: image1,
+			favorite: true,
+		},
+		{
+			id: 4,
+			category: "women's clothing",
+			name: "Opna Women's Short Sleeve Moisture",
+			rating: 4.5,
+			price: 137.95,
+			image: image1,
+		},
+		{
+			id: 5,
+			category: "women's clothing",
+			name: "Opna Women's Short Sleeve Moisture",
+			rating: 4.5,
+			price: 137.95,
+			image: image1,
+		},
 	]
 	return (
 		<div className={`card-list ${layout === 'horizontal' ? 'card-list--horizontal' : ''}`}>
